c1388cbc92010e47dcb37093047ac74cab9b522d
Mikhail <wordoftanks387@gmail.com>
2024-12-20 14:08:12 +0300

    first commit

---
+++ src/components/utils/apiFindings.js
@@ -0,0 +1,10 @@
+// Объект для передачи(получаения) данных на сервер(сервера)
+const apiFindings = {
+  link: 'https://nomoreparties.co/v1/apf-cohort-202/',
+  headers: {
+    authorization: '35aed384-89c2-4aad-aff8-cf5191d47650',
+    'Content-Type': 'application/json'
+  }
+}
+// Экспортируем объект в index.js
+export { apiFindings };
